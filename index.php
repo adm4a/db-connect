963da63646dae14b7dedb0b1a95380cb4548f592
Adle <admir.nedzibovic@stu.ibu.edu.ba>
2023-03-25 18:01:02 +0100

    Third commit

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
 <?php
-
-require_once("rest/dao/ProjectDao.class.php");
-$project_dao = new ProjectDao();
-
-$results = $project_dao->get_all();
-print_r($results);
-
-/*header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS, PATCH');
 require 'vendor/autoload.php';
 
@@ -18,7 +11,7 @@ try{
     echo "Error". $e->getMessage();
 }
 
-echo "Hello there!"*/
+echo "Hello there!"
 
 
 
--- rest/dao/ProjectDao.class.php
+++ rest/dao/ProjectDao.class.php
@@ -20,5 +20,21 @@
             return $stmt->fetchAll(PDO::FETCH_ASSOC);
 
         }
+
+        public function add_elements($first_name, $last_name, $age){
+            $stmt = $this->conn->prepare("INSERT INTO Users (first_name, last_name, age) VALUES ('user001', 'test001', 'age001')");
+            $result = $stmt->exec(['first_name'=>$first_name,'last_name'=>$last_name,  'age'=>$age]);
+        }
+
+        public function update_elements($first_name, $last_name, $age, $id){
+            $stmt = $this->conn->prepare("UPDATE Users SET first_name = ':first_name', last_name = ':last_name', age = ':age' WHERE id=:id");
+            $stmt->exec(['first_name'=>$first_name,'last_name'=>$last_name,  'age'=>$age, 'id'=>$id]);
+        }
+
+        public function delete_elements($id){
+            $stmt = $this->conn->prepare("DELETE FROM Users WHERE id=:id");
+            $stmt->bindParam(':id', $id);
+            $stmt->exec();
+        }
     }
 ?>
