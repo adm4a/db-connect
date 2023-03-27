611385c0fd8615853cefb34f4975b5e2bf7ceeeb
Adle <admir.nedzibovic@stu.ibu.edu.ba>
2023-03-27 13:30:29 +0200

    Commitara2

--- rest/dao/ProjectDao.class.php
+++ rest/dao/ProjectDao.class.php
@@ -20,17 +20,18 @@ class ProjectDao{
     }
 
     public function add($first_name, $last_name, $age){
-        $stmt = $this->conn->prepare("INSERT INTO Users (first_name, last_name, age) VALUES ($first_name, $last_name, $age");
-        $result = $stmt->execute(); 
+        $stmt = $this->conn->prepare("INSERT INTO Users (first_name, last_name, age) VALUES (':first_name', ':last_name' , ':age')");
+        $result = $stmt->execute(['first_name'=> $first_name, 'last _name' => $last_name, 'age' => $age]);
     }
 
     public function update($first_name, $last_name, $age, $id){
-        $stmt = $this->conn->prepare("UPDATE Users SET first_name=':first_name', last_name=':last_name', age=':age' WHERE id=:id ");
-        $stmt->execute(); 
+        $stmt = $this->conn->prepare("UPDATE Users SET first_name=':first_name', last_name= ':last_name', age=':age' WHERE id=:id ");
+        $stmt->execute(['first_name'=> $first_name, 'last _name' => $last_name, 'age' => $age, 'id' => $id]);
     }
 
     public function delete($id){
         $stmt = $this->conn->prepare("DELETE FROM Users WHERE id = $id");
+        $stmt-> bindParam(':id', $id);
         $stmt->execute(); 
     }
 }
